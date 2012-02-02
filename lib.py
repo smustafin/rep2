03044ddf9466eb26ea458fe692a735a1f8ad1ccc
shoma <mustafin_shamil@yahoo.com>
2012-02-02 16:01:47 +0400

    chutok

--- lib.py
+++ lib.py
@@ -7,66 +7,61 @@ except IOError:
     exit()
 lin=f.readlines()
 f.close()
-if lin[0][0:6]=="login=":
-    login=lin[0][6:]
-if lin[1][0:5]=="pass=":
-    passw=lin[1][5:]
-if lin[2][0:9]=="org_name=":
-    nameorg=lin[2][9:]
+st=lin[0].split("=")[1]
+login=st[0:len(st)-1]
+st=lin[1].split("=")[1]
+passw=st[0:len(st)-1]
+st=lin[2].split("=")[1]
+org_name=st[0:len(st)-1]
 try:
-    tm1,tm2,tm3=login, passw, nameorg
-    print tm1,tm2,tm3
+    tm1,tm2,tm3=login, passw, org_name
 except NameError:
     print "Bad srtucture 'conf' file"
     exit()
 host="https://api.github.com/"
-login, passw, nameorg="smustafin","12as34df","orgtest1"
 #delete user from org
 def del_from_org(user):
-    reqq='orgs/%s/members/%s' % (nameorg,user)
+    reqq='orgs/%s/members/%s' % (org_name,user)
     url=host + reqq
     r = requests.delete(url, auth=(login,passw))
     if r.status_code==204:
-        resp="User "+user+" was deleted"
+        res="User "+user+" was deleted"
     else:
-        resp="Error "+ r.headers['status']  
-    return resp
-#user=raw_input("Pass username: ") 
-#print delete(user)
-#create team\
+        res="Error "+ r.headers['status']  
+    return res
+#create team
 def create_team(team_name,permission,repo_name):
-    reqq = 'orgs/%s/teams' % nameorg
+    reqq = 'orgs/%s/teams' % org_name
     url = host + reqq
     try:
-        r = requests.post(url,auth = (login,passw),data = '{"name":"%s", "repo_names":["%s/%s"], "permission":"%s"}' % (team_name,nameorg,repo_name,permission))
+        r = requests.post(url,auth = (login,passw),data = '{"name":"%s", "repo_names":["%s/%s"], "permission":"%s"}' % (team_name,org_name,repo_name,permission))
     except r.status_code != 201:
         return "Error "+ r.headers['status']
     else:
         return "%s was created" % team_name
 # create repo
-def create_repo(namerep,descrip): #withot private
+def create_repo(namerep,descrip): #without private
     #tmp=private
     #if (tmp<>"true"):
     # if (tmp<>"false"):
     #  print "Second parametr could be 'true' or 'false'"
     # else: 
-    reqq='orgs/%s/repos' % (nameorg)
+    reqq='orgs/%s/repos' % (org_name)
     url=host + reqq
     tmp='{"name":"%s","description":"%s"}' % (namerep,descrip)
     r = requests.post(url, auth=(login,passw),data=tmp)
     if r.status_code==201:
-        res="Done!!! Repo '"+namerep+"' created"
+        res="Done!!! Repo '%s' created" % (namerep)
         # creat 3 teams
         create_team(namerep,"pull",namerep)
         create_team(namerep+"-guests","push",namerep)
-        create_team(namerep+"-owners","admin",namerep)
-  
+        create_team(namerep+"-owners","admin",namerep) 
     else:
         res="Error "+ r.headers['status']  
     return res
 #search id_team by name
 def search_id_team(team_name):
-    reqq='orgs/%s/teams' % (nameorg)
+    reqq='orgs/%s/teams' % (org_name)
     url=host + reqq
     r = requests.get(url, auth=(login,passw))
     cont=json.loads(r.content)
@@ -79,7 +74,7 @@ def search_id_team(team_name):
         except IndexError:
             return "Team not found"            
     return cont[i]['id']  
-#  
+#  delete member from org
 def del_from_team(user,team_name):
     #/teams/:id/members/:user
     if search_id_team(team_name)=="Team not found":
@@ -92,6 +87,4 @@ def del_from_team(user,team_name):
     else:
         res="Error "+ r.headers['status']  
     return res
-#print del_from_team("eg","repoo")
-    
-
+#print search_id_team("Owners")
