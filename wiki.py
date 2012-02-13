405e0764513b34feac46743845c7fa14fcd4b707
Shom <shom@shom-N50Vn.(none)>
2012-02-14 00:46:16 +0400

    cooool

--- wiki.py
+++ wiki.py
@@ -1,40 +1,43 @@
 import xmlrpclib
 import libvirt
 SPACE='ITST'
-TOP_PAGE='Proba'
+TOP_PAGE='Home' #name top page
 WIKI_USER='smustafin'
 WIKI_PASS='12as34df'
 p = "h1." + "TEST" + "\n"
 p = p + "||id||date||title||author||samary||\n"
-# cycle
+# cycle for, or while 
 vm={
-		"id":"000003",
-		"date":"01120112",
-		"title":"12312",
+		"id":"00000",# value data sqlite
+		"date":"000001we12",
+		"title":"12000002",
 		"author":"qweqw",
-		"samary":"sdfsdf"
+		"samary":"sdf0000gsdf"
 	} 
 p=p+"|["+vm["id"]  + "|" + vm["date"]+"|"+vm["title"]+"|"+vm["author"]+"|"+vm["samary"]+"|\n"
-p=p+"|["+"))"+ "|" +"33"+ "|" + "134"+ "|" + "sdfjh"+ "|"+ "wert"+ "|"
+#end cycle
 print p
-#conn = libvirt.openReadOnly('qemu:///system')
-host = "smustafin"
+
+host = "Well Done Python 222" # name news
 print host
 
 server = xmlrpclib.ServerProxy('https://wiki.griddynamics.net/rpc/xmlrpc');
 token = server.confluence1.login(WIKI_USER, WIKI_PASS);
 print token
 try:
-	page = server.confluence1.getPage(token, SPACE, host);
+	page = server.confluence1.getPage(token, SPACE, host); 
+	# if page no exists, then go to except
+	
 except:
-	#parent = server.confluence1.getPage(token, SPACE, TOP_PAGE);
+	parent = server.confluence1.getPage(token, SPACE, TOP_PAGE); #create page
 	page={
-		'parentId': "71501338",
+		'parentId': parent['id'],
 		'space': SPACE,
 		'title': host,
 		'content': p
 		 }
 	server.confluence1.storePage(token, page);
 else:
-	page['content'] = p;
+	page['content'] = p; #continue try (if page exists)
 	server.confluence1.updatePage(token, page,{'versionComment':'','minorEdit':1});
+	#weelll done)))) 
