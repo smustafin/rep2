10d4a7ac1eed77e16a55b9682c178d0f2076f273
shoma <mustafin_shamil@yahoo.com>
2012-02-13 20:15:25 +0400

    mutki

--- con.py
+++ con.py
@@ -2,36 +2,36 @@ import requests
 import json
 log="smustafin"
 pasw="12as34df"
-usr="smustafin"
-reqq=""
-user=raw_input("Pass username: ")
+res=""
 while 1:
  try:
   ch=input("""Pass 1 and Enter for show info about user\nPass 2 and Enter for show user`s repos\n""")
-  if ch==1: 
-   reqq='/users/%s' % (user)  
-   break
-  if ch==2: 
-   reqq='/users/%s/repos' % (user) 
-   print "wait..."
+  if (ch==1)|(ch==2): 
    break
   print "Pass again...."
  except NameError:  
   print "Pass again...."
  except SyntaxError:  
   print "Pass again...."
-print "wait..."
-host="https://api.github.com%s" % (reqq)
-r = requests.get(host, auth=(log,pasw))
-print r.headers['content-type']
-print "\n"
+while res!="Done":
+ user=raw_input("Pass username: ")
+ if ch==1:
+   reqq='/users/%s' % (user)  
+ if ch==2: 
+   reqq='/users/%s/repos' % (user)
+ print "wait..."
+ host="https://api.github.com%s" % (reqq)
+ r = requests.get(host, auth=(log,pasw))
+ slov=json.loads(r.content)
+ try:    
+  if slov['message']!="":
+   print "username ",slov['message']
+   res="Fail"
+ except KeyError:
+  res="Done"
+ except TypeError:
+  res="Done" 
+print
 print r.content
-print "\n"
-# all content divided on small part )
-slov=json.loads(r.content)
-try:
- print "email=", slov['email']
-except KeyError:
- print "Not found"
 #weiuweoriuweior
  
